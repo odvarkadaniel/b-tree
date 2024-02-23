ceeee622836aec784ad0b6f4cf23f55f6f58a6e2
Daniel Odvarka <odvarkaadaniel@gmail.com>
2024-02-23 22:12:50 +0100

    malloc btree object

--- btree.c
+++ btree.c
@@ -1,6 +1,37 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <stdint.h>
 
-int main()
+#include "btree.h"
+
+static size_t btree_fit_size(size_t item_sz)
 {
-    printf("Hello, World!\n");
+    size_t size = (sizeof(btree));
+    size += item_sz;
+
+    return size;
+}
+
+btree *btree_new(size_t item_sz, size_t max_items, int (*comparator)(void *, void *))
+{
+    size_t size = btree_fit_size(item_sz);
+    btree *btree = malloc(size);
+    if (!btree)
+    {
+        return NULL;
+    }
+
+    btree->count = 0;
+    btree->height = 0;
+    btree->max_items = max_items;
+    btree->min_items = max_items / 2;
+    btree->comparator = comparator;
+
+    return btree;
+}
+
+void btree_free(btree *btree)
+{
+    // free(btree->root);
+    free(btree);
 }
--- btree.h
+++ btree.h
@@ -2,24 +2,26 @@
 #ifdef BTREE_H
 
 #include <stdlib.h>
+#include <stdbool.h>
 
 typedef struct
 {
     bool leaf;
     int nitems; // : 16; // or something else than 16
     char *items;
-    bnode *children[];
+    struct bnode *children[];
 } bnode;
 
 typedef struct
 {
+    // We don't care about these custom allocators for now.
     void *(*malloc)(size_t);
     void *(*remalloc)(size_t);
     void *(*free)(void *);
 
     int (*comparator)(void *, void *);
 
-    size_t order;     // number of items in the tree
+    size_t count;     // number of items in the tree
     size_t height;    // height of the tree
     bnode *root;      // root of the tree
     size_t max_items; // max items allowed per node before splitting
@@ -27,4 +29,18 @@ typedef struct
 
 } btree;
 
-#endif BTREE_H
+btree *btree_new(size_t item_sz, size_t max_items, int (*comparator)(void *, void *));
+
+static size_t btree_fit_size(size_t item_sz);
+
+void btree_free(btree *btree);
+
+void btree_clear(btree *btree);
+
+const void *btree_min(const btree *btree);
+
+const void *btree_max(const btree *btree);
+
+void btree_print(btree *btree);
+
+#endif // BTREE_H
